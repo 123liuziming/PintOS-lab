515fa11480e2636d1dd123997b955cf7d5b9703e
liuziming <448918299@qq.com>
2020-09-22 15:02:22 +0800

    add comment

--- cs140-ps0/caltrain.c
+++ cs140-ps0/caltrain.c
@@ -31,7 +31,17 @@ station_wait_for_train(struct station *station)
 {
 	// 等待cond_empty
 	lock_acquire(station->lock);
-	cond_wait(station->cond_empty, station->lock);
+	/**
+	 * pthread_cond_wait的工作机制如下:
+	 * 1. 阻塞等待条件变量cond满足
+	 * 2. 解除已绑定的互斥锁
+	 * 3. 当线程被唤醒时,pthread_cond_wait函数返回,该函数同时会解除线程阻塞并使线程重新申请绑定互斥锁
+	 * 其中1, 2味原子操作,但是3表明线程唤醒后还需要重新绑定互斥锁
+	 * 极有可能在线程B绑定互斥锁之前,线程A已执行了
+	 * 获取互斥锁->修改条件变量->解除互斥锁，此时B获取到互斥锁，但是条件变量不能满足，还要继续阻塞等待
+	 **/
+	while (station->seat_num <= 0)
+		cond_wait(station->cond_empty, station->lock);
 	station_on_board(station);
 	lock_release(station->lock);
 }
