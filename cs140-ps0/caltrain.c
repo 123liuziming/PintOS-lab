44c341eaa112062c0355887f014be5a8076941d6
liuziming <448918299@qq.com>
2020-09-24 22:55:02 +0800

    caltrain test case passed

--- cs140-ps0/caltrain.c
+++ cs140-ps0/caltrain.c
@@ -1,17 +1,32 @@
 #include "pintos_thread.h"
 
 struct station {
+	// 火车上的空闲座位
 	int seat_num;
+	// 火车站正在等待的乘客
+	int wait_passengers;
+	// 上火车了的乘客
+	int board_passengers;
+	// 互斥锁
 	struct lock *lock;
-	struct condition *cond_empty;
+	// 火车座位坐满
+	struct condition *cond_full;
+	// 有新火车来到
+	struct condition *cond_train;
 };
 
 void
 station_init(struct station *station)
 {
 	station->seat_num = 0;
+	station->wait_passengers = 0;
+	station->board_passengers = 0;
+	station->lock = malloc(sizeof(struct lock));
+	station->cond_full = malloc(sizeof(struct condition));
+	station->cond_train = malloc(sizeof(struct condition));
 	lock_init(station->lock);
-	cond_init(station->cond_empty);
+	cond_init(station->cond_full);
+	cond_init(station->cond_train);
 }
 
 void
@@ -21,8 +36,14 @@ station_load_train(struct station *station, int count)
 	station->seat_num += count;
 	// 通知此station已经有座位了
 	// 有座位了才通知释放锁
-	if (station->seat_num > 0)
-		cond_broadcast(station->cond_empty, station->lock);
+	while (station->seat_num > 0 && station->wait_passengers > 0) {
+		cond_broadcast(station->cond_train, station->lock);
+		// 全坐满了,可以出发了
+		cond_wait(station->cond_full, station->lock);
+	}
+	// 出发后把座位重设为0,表示车已经开走了
+	// 注意,没有乘客在等的时候会直接开走
+	station->seat_num = 0;
 	lock_release(station->lock);
 }
 
@@ -40,14 +61,21 @@ station_wait_for_train(struct station *station)
 	 * 极有可能在线程B绑定互斥锁之前,线程A已执行了
 	 * 获取互斥锁->修改条件变量->解除互斥锁，此时B获取到互斥锁，但是条件变量不能满足，还要继续阻塞等待
 	 **/
-	while (station->seat_num <= 0)
-		cond_wait(station->cond_empty, station->lock);
-	station_on_board(station);
+	++(station->wait_passengers);
+	while (station->board_passengers == station->seat_num)
+		cond_wait(station->cond_train, station->lock);
+	--(station->wait_passengers);
+	++(station->board_passengers);
 	lock_release(station->lock);
 }
 
 void
 station_on_board(struct station *station)
 {
-	--(station->seat_num); 
+	lock_acquire(station->lock);
+	--(station->board_passengers);
+	--(station->seat_num);
+	if (station->seat_num == 0 || station->board_passengers == 0)
+		cond_signal(station->cond_full, station->lock);
+	lock_release(station->lock);
 }
