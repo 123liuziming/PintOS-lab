b3cb54f1adb38e36d9d48c9d7ab58488b5312caf
liuziming <448918299@qq.com>
2020-09-24 11:16:47 +0800

    xjbx

--- cs140-ps0/reaction.c
+++ cs140-ps0/reaction.c
@@ -9,7 +9,8 @@ struct reaction {
 	int h_num;
 	int o_num;
 	struct lock *lock;
-	struct condition *cond_not_ready;
+	struct condition *h_cond_not_ready;
+	struct condition *o_cond_not_ready;
 };
 
 void
@@ -18,7 +19,8 @@ reaction_init(struct reaction *reaction)
 	reaction->h_num = 0;
 	reaction->o_num = 0;
 	lock_acquire(reaction->lock);
-	cond_init(reaction->cond_not_ready);
+	cond_init(reaction->h_cond_not_ready);
+	cond_init(reaction->o_cond_not_ready);
 }
 
 void
@@ -26,14 +28,17 @@ reaction_h(struct reaction *reaction)
 {
 	lock_acquire(reaction->lock);
 	++(reaction->h_num);
-	while (reaction->o_num < 2)
-		cond_wait(reaction->cond_not_ready, reaction->lock);
-	reaction->o_num -= 2;
-	--reaction->h_num;
-	if (reaction->h_num >= 0) {
-		cond_signal(reaction->cond_not_ready, reaction->lock);
+	while (reaction->h_num < 2)
+		cond_wait(reaction->h_cond_not_ready, reaction->lock);
+	while (reaction->o_num < 1)
+		cond_wait(reaction->o_cond_not_ready, reaction->lock);
+	if (reaction->h_num >= 2 && reaction->o_num >= 1) {
+		reaction->h_num -= 2;
+		--reaction->o_num;
 		make_water();
 	}
+	if (reaction->h_num >= 2)
+		cond_signal(reaction->h_cond_not_ready, reaction->lock);
 	lock_release(reaction->lock);
 }
 
@@ -42,13 +47,16 @@ reaction_o(struct reaction *reaction)
 {
 	lock_acquire(reaction->lock);
 	++(reaction->o_num);
-	while (reaction->h_num < 1)
-		cond_wait(reaction->cond_not_ready, reaction->lock);
-	reaction->o_num -= 2;
-	--reaction->h_num;
-	if (reaction->o_num >= 0) {
-		cond_signal(reaction->cond_not_ready, reaction->lock);
+	while (reaction->o_num < 1)
+		cond_wait(reaction->o_cond_not_ready, reaction->lock);
+	while (reaction->h_num < 2)
+		cond_wait(reaction->h_cond_not_ready, reaction->lock);
+	if (reaction->h_num >= 2 && reaction->o_num >= 1) {
+		reaction->h_num -= 2;
+		--reaction->o_num;
 		make_water();
 	}
+	if (reaction->o_num >= 1)
+		cond_signal(reaction->o_cond_not_ready, reaction->lock);
 	lock_release(reaction->lock);
 }
