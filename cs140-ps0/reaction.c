ecc0dde1d03515ce30f5daca676275e1855c6eca
liuziming <448918299@qq.com>
2020-09-24 11:06:24 +0800

    瞎jb写

--- cs140-ps0/reaction.c
+++ cs140-ps0/reaction.c
@@ -6,23 +6,49 @@
 void make_water();
 
 struct reaction {
-	// FILL ME IN
+	int h_num;
+	int o_num;
+	struct lock *lock;
+	struct condition *cond_not_ready;
 };
 
 void
 reaction_init(struct reaction *reaction)
 {
-	// FILL ME IN
+	reaction->h_num = 0;
+	reaction->o_num = 0;
+	lock_acquire(reaction->lock);
+	cond_init(reaction->cond_not_ready);
 }
 
 void
 reaction_h(struct reaction *reaction)
 {
-	// FILL ME IN
+	lock_acquire(reaction->lock);
+	++(reaction->h_num);
+	while (reaction->o_num < 2)
+		cond_wait(reaction->cond_not_ready, reaction->lock);
+	reaction->o_num -= 2;
+	--reaction->h_num;
+	if (reaction->h_num >= 0) {
+		cond_signal(reaction->cond_not_ready, reaction->lock);
+		make_water();
+	}
+	lock_release(reaction->lock);
 }
 
 void
 reaction_o(struct reaction *reaction)
 {
-	// FILL ME IN
+	lock_acquire(reaction->lock);
+	++(reaction->o_num);
+	while (reaction->h_num < 1)
+		cond_wait(reaction->cond_not_ready, reaction->lock);
+	reaction->o_num -= 2;
+	--reaction->h_num;
+	if (reaction->o_num >= 0) {
+		cond_signal(reaction->cond_not_ready, reaction->lock);
+		make_water();
+	}
+	lock_release(reaction->lock);
 }
