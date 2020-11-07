cde398aff6a8155cbe68beda84b22ccc85f6caf3
liuziming <448918299@qq.com>
2020-11-07 22:10:23 +0800

    args passing finish

---
+++ project2-user_programs/src/userprog/syscall.c
@@ -0,0 +1,37 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+
+static void syscall_handler (struct intr_frame *);
+
+void
+syscall_init (void) 
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+static void
+syscall_handler (struct intr_frame *f UNUSED) 
+{
+  int * p = f->esp;
+
+  int system_call = * p;
+
+	switch (system_call)
+	{
+		case SYS_WRITE:
+		if(*(p+5)==1)
+		{
+			putbuf(*(p+6),*(p+7));
+		}
+		break;
+		case SYS_EXIT:
+		thread_exit();
+		break;
+
+		default:
+		printf("No match\n");
+	}
+}
