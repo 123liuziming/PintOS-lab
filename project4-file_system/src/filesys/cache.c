b605f6100fe169d5882973ffc46c4882dd9f65af
liuziming <448918299@qq.com>
2021-01-02 21:26:08 +0800

    cache

--- project4-file_system/src/filesys/cache.c
+++ project4-file_system/src/filesys/cache.c
@@ -1,2 +1,71 @@
 #include "filesys/cache.h"
 
+static unsigned cache_hash_func(const struct hash_elem* elem, void* aux) {
+  struct cache_entry* entry = hash_entry(elem, struct cache_entry, hash_elem);
+  return hash_int((int)entry->block_num);
+}
+
+
+static bool cache_less_func(const struct hash_elem *a, const struct hash_elem *b, void *aux) {
+  struct cache_entry *a_entry = hash_entry(a, struct cache_entry, hash_elem);
+  struct cache_entry *b_entry = hash_entry(b, struct cache_entry, hash_elem);
+  return a_entry->block_num < b_entry->block_num;
+}
+
+static void
+cache_destroy_func(struct hash_elem *elem, void *aux)
+{
+  struct cache_entry *entry = hash_entry(elem, struct cache_entry, hash_elem);
+  free (entry);
+}
+
+struct list_elem* now = NULL;
+
+static void next_element() {
+    if (now == NULL || now == list_end(&cache_list))
+        now = list_begin(&cache_list);
+    else
+        now = list_next(now);
+}
+
+// 时钟算法
+static struct cache_entry* clock_eviction() {
+    int n = list_size(&cache_list);
+    int i = 0;
+    for (; i <= n; ++i) {
+        struct cache_entry* entry = list_entry(now, struct cache_entry, list_elem);
+        if (entry->is_accessed)
+            entry->is_accessed = false;
+        else
+            return entry;
+    }
+    return NULL;
+}
+
+static struct cache_entry* cache_lookup(block_sector_t block) {
+    struct cache_entry tmp;
+    tmp.block_num = block;
+    struct hash_elem* elem = hash_find(&cache_map, &tmp.hash_elem);
+    if (elem == NULL)
+        return NULL;
+    return hash_entry(elem, struct cache_entry, hash_elem);
+}
+
+void cache_init() {
+    hash_init(&cache_map, cache_hash_func, cache_less_func, NULL);
+    list_init(&cache_list);
+}
+
+void cache_destroy() {
+    hash_destroy(&cache_map, cache_destroy_func);
+}
+
+void cache_read(block_sector_t block, void* addr) {    
+    struct cache_entry *entry = cache_lookup(block);
+    // 页不存在,则从磁盘里读,读完创建缓存条目
+    if (!entry) {
+
+    } else {
+        // 直接复制缓存块到目标地址
+    }
+}
--- project4-file_system/src/filesys/cache.h
+++ project4-file_system/src/filesys/cache.h
@@ -1,13 +1,25 @@
 #ifndef CACHE_H
 
 #include "devices/block.h"
+#include <hash.h>
+#include <list.h>
+
+struct cache_entry {
+    block_sector_t block_num;
+    struct hash_elem hash_elem;
+    struct list_elem list_elem;
+    // 脏位
+    bool is_dirty;
+    // 用于时钟算法
+    bool is_accessed;
+};
+
+struct hash cache_map;
+struct list cache_list;
 
 void cache_init();
-
 void cache_read(block_sector_t block, void* addr);
-
 void cache_write(block_sector_t block, const void* addr);
-
 void cache_destroy();
 
 #endif
